27f1912f32b78b854c088451baed28d7fe5207a5
zhangqiangoffice <zhangqiangoffice@163.com>
2016-11-28 23:18:50 +0800

    登出接口

--- routes/member.js
+++ routes/member.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var dburl = 'mongo:ds111748.mlab.com:11748/zhanglemeng';
+
+var insertDB = function(db, callback) {
+    var collection = db.collection('ablum');
+    collection.insert({"foo" : "bar"}, function(err, result){
+        callback(result.result);
+    });
+};
 
 //用户登录
 router.get('/login', function(req, res) {
@@ -27,6 +36,23 @@ router.get('/index', function(req, res) {
         res.redirect('./login');
     }
     res.render('member/index', {user: req.session.user});
+});
+
+//登出接口
+router.post('/logout', function(req, res) {
+    req.session.user = false;
+    res.json({result: 1, message: '成功'});
+});
+
+//操作数据库
+router.post('/insert', function(req, res) {
+    MongoClient.connect(dburl, function(err, db) {
+        insertDB(db, function(result) {
+            res.json({result: 1, message: '成功'});          
+            db.close();
+        });
+    });
+    
 });
 
 module.exports = router;
